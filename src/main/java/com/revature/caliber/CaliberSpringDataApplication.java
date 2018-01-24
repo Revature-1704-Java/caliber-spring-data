dc55d257ec5cc042022c2642f16108a5105cd391
Kei Peralta <cnmperalta@gmail.com>
2018-01-24 13:24:04 -0500

    Initial commit with Spring Boot application

---
+++ src/main/java/com/revature/caliber/CaliberSpringDataApplication.java
@@ -0,0 +1,12 @@
+package com.revature.caliber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaliberSpringDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaliberSpringDataApplication.class, args);
+	}
+}
