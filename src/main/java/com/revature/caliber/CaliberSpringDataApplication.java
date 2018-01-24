fdb2535fe3040310c8ba21fa9533791e5469ebae
Kei Peralta <cnmperalta@gmail.com>
2018-01-24 15:36:17 -0500

    Add AddressRepository

--- src/main/java/com/revature/caliber/CaliberSpringDataApplication.java
+++ src/main/java/com/revature/caliber/CaliberSpringDataApplication.java
@@ -1,12 +1,32 @@
 package com.revature.caliber;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
+
+import com.revature.caliber.model.Address;
+import com.revature.caliber.repository.AddressRepository;
 
 @SpringBootApplication
 public class CaliberSpringDataApplication {
+	
+	@Autowired
+	private AddressRepository addressRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CaliberSpringDataApplication.class, args);
+	}
+	
+	@Bean
+	public CommandLineRunner runner() {
+		return args -> {
+			List<Address> addresses = addressRepository.findAll();
+			System.out.println(addresses);
+		};
 	}
 }
