3fdaba64d8ebfa8c0e2937cccbe2893faa9486c4
Fenrir2539 <Fenrir2539@users.noreply.github.com>
2018-01-25 11:13:25 -0500

    Actually got the dialect free queries working

---
+++ src/test/java/com/revature/caliber/data/CategoryDAOTest.java
@@ -0,0 +1,35 @@
+package com.revature.caliber.data;
+
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.revature.caliber.beans.Address;
+import com.revature.caliber.beans.Category;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class CategoryDAOTest {
+	@Autowired
+    private TestEntityManager entityManager;
+	@Autowired
+	CategoryDAO dao;
+	
+	@Test
+	public void testAll() {
+		List<Category> test = dao.findAll();
+		Assert.assertTrue(test.isEmpty());
+	}
+	
+	@Test
+	public void testAllActive() {
+		List<Category> test = dao.findAllActive();
+		Assert.assertTrue(test.isEmpty());
+	}
+}
